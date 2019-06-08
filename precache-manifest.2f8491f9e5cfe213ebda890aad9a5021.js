56795b2a2570b7be24aed4603a255f87f1bc0bf7
abulinski <arbulinski53@gmail.com>
2019-06-08 12:48:33 -0400

    Updates

---
+++ precache-manifest.2f8491f9e5cfe213ebda890aad9a5021.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3985171a57b2ef25b75932f83696bfb",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "eb32ad6e24f6d0feaf09",
+    "url": "/contactmanager/static/css/2.b9dc5a1e.chunk.css"
+  },
+  {
+    "revision": "12a6f7e041125bc36a6b",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "eb32ad6e24f6d0feaf09",
+    "url": "/contactmanager/static/js/2.71ddfead.chunk.js"
+  },
+  {
+    "revision": "12a6f7e041125bc36a6b",
+    "url": "/contactmanager/static/js/main.6990225d.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
